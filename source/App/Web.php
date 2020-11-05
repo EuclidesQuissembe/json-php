095468ad868ba901f2b8de8f97cb062e0d9f434c
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2020-11-05 14:56:44 +0100

    :zap: Web Controller added

---
+++ source/App/Web.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace source\App;
+
+use Source\Core\Controller;
+
+/**
+ * JSON-PHP | Class Web
+ *
+ * @author Euclides Bernardo <euclidesquissembebernado@gmail.com>
+ * @package source\App
+ */
+class Web extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct(CONF_VIEW_PATH . CONF_VIEW_THEME);
+    }
+
+    public function home(): void
+    {
+        $head = $this->seo(
+            CONF_SITE_NAME,
+            CONF_SITE_DESC,
+            url('/'),
+            ''
+        );
+
+        echo $this->view->render('widgets/home/index', [
+            'head' => $head
+        ]);
+    }
+
+    public function error(array $data): void
+    {
+        echo "${data['errCode']}";
+    }
+}
