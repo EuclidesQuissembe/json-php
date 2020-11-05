9cd3dec7981a238c4a912ae94735f539a3ebcde2
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2020-11-05 14:50:37 +0100

    :zap: Boot

--- source/Boot/Config.php
+++ source/Boot/Config.php
@@ -1,1 +1,94 @@
 <?php
+/**o dia?
+ * DATABASE
+ */
+define("CONF_DB_HOST", "localhost");
+define("CONF_DB_USER", "root");
+define("CONF_DB_PASS", "");
+define("CONF_DB_NAME", "enam");
+
+/**
+ * PROJECT URLs
+ */
+define("CONF_URL_BASE", "http://localhost/json-php");
+define("CONF_URL_TEST", "http://localhost/json-php");
+
+/**
+ * SITE
+ */
+define("CONF_SITE_NAME", "JSON File ");
+define("CONF_SITE_TITLE", "Escola Nacional de Migração");
+define("CONF_SITE_DESC", "ENAM");
+define("CONF_SITE_LANG", "pt_AO");
+define("CONF_SITE_DOMAIN", "enam.co.ao");
+define("CONF_SITE_ADDR_STREET", "SC 406 - Rod. Drº Antônio Luiz Moura Gonzaga");
+define("CONF_SITE_ADDR_NUMBER", "3339");
+define("CONF_SITE_ADDR_COMPLEMENT", "Bloco A, Sala 208");
+define("CONF_SITE_ADDR_CITY", "Luanda");
+define("CONF_SITE_ADDR_STATE", "AO");
+define("CONF_SITE_ADDR_ZIPCODE", "88048-301");
+
+/**
+ * SOCIAL
+ */
+define("CONF_SOCIAL_TWITTER_CREATOR", "@creator");
+define("CONF_SOCIAL_TWITTER_PUBLISHER", "@creator");
+define("CONF_SOCIAL_FACEBOOK_APP", "5555555555");
+define("CONF_SOCIAL_FACEBOOK_PAGE", "pagename");
+define("CONF_SOCIAL_FACEBOOK_AUTHOR", "author");
+define("CONF_SOCIAL_GOOGLE_PAGE", "5555555555");
+define("CONF_SOCIAL_GOOGLE_AUTHOR", "5555555555");
+define("CONF_SOCIAL_INSTAGRAM_PAGE", "insta");
+define("CONF_SOCIAL_YOUTUBE_PAGE", "youtube");
+
+/**
+ * DATES
+ */
+define("CONF_DATE_BR", "d/m/Y H:i:s");
+define("CONF_DATE_APP", "Y-m-d H:i:s");
+
+/**
+ * PASSWORD
+ */
+define("CONF_PASSWD_MIN_LEN", 8);
+define("CONF_PASSWD_MAX_LEN", 40);
+define("CONF_PASSWD_ALGO", PASSWORD_DEFAULT);
+define("CONF_PASSWD_OPTION", ["cost" => 10]);
+
+/**
+ * VIEW
+ */
+define("CONF_VIEW_PATH", __DIR__ . "/../../themes/");
+define("CONF_VIEW_EXT", "php");
+define("CONF_VIEW_THEME", "chartsweb");
+define("CONF_VIEW_ADMIN", "admin");
+
+/**
+ * UPLOAD
+ */
+define("CONF_UPLOAD_DIR", __DIR__ . "/../../storage");
+define("CONF_UPLOAD_IMAGE_DIR", "images");
+define("CONF_UPLOAD_FILE_DIR", "files");
+define("CONF_UPLOAD_MEDIA_DIR", "medias");
+
+/**
+ * IMAGES
+ */
+define("CONF_IMAGE_CACHE", CONF_UPLOAD_DIR . "/" . CONF_UPLOAD_IMAGE_DIR . "/cache");
+define("CONF_IMAGE_SIZE", 2000);
+define("CONF_IMAGE_QUALITY", ["jpg" => 75, "png" => 5]);
+
+/**
+ * MAIL
+ */
+define("CONF_MAIL_HOST", "smtp.sendgrid.net");
+define("CONF_MAIL_PORT", "587");
+define("CONF_MAIL_USER", "apikey");
+define("CONF_MAIL_PASS", "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");
+define("CONF_MAIL_SENDER", ["name" => "Robson V. Leite", "address" => "sender@email.com"]);
+define("CONF_MAIL_SUPPORT", "sender@support.com");
+define("CONF_MAIL_OPTION_LANG", "br");
+define("CONF_MAIL_OPTION_HTML", true);
+define("CONF_MAIL_OPTION_AUTH", true);
+define("CONF_MAIL_OPTION_SECURE", "tls");
+define("CONF_MAIL_OPTION_CHARSET", "utf-8");
--- source/Boot/Helpers.php
+++ source/Boot/Helpers.php
@@ -1,1 +1,486 @@
 <?php
+
+/**
+ * ####################
+ * ###   VALIDATE   ###
+ * ####################
+ */
+
+/**
+ * @param string $email
+ * @return bool
+ */
+function is_email(string $email): bool
+{
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+/**
+ * @param string $password
+ * @return bool
+ */
+function is_passwd(string $password): bool
+{
+    if (password_get_info($password)['algo'] || (mb_strlen($password) >= CONF_PASSWD_MIN_LEN && mb_strlen($password) <= CONF_PASSWD_MAX_LEN)) {
+        return true;
+    }
+
+    return false;
+}
+
+/**
+ * @param string $phone
+ * @param string $country
+ * @return bool
+ */
+function is_phone(string $phone, string $country = "ao"): bool
+{
+    $country = strtolower($country);
+    switch ($country) {
+        case "ao":
+            $pattern = "/^9[1-49][\d]{7}$/";
+            if (preg_match($pattern, $phone, $matches)) {
+                return true;
+            }
+            break;
+        default:
+            return true;
+    }
+    return false;
+}
+
+/**
+ * ##################
+ * ###   STRING   ###
+ * ##################
+ */
+
+/**
+ * @param string $string
+ * @return string
+ */
+function str_slug(string $string): string
+{
+    $string = filter_var(mb_strtolower($string), FILTER_SANITIZE_STRIPPED);
+    $formats = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜüÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿRr"!@#$%&*()_-+={[}]/?;:.,\\\'<>°ºª';
+    $replace = 'aaaaaaaceeeeiiiidnoooooouuuuuybsaaaaaaaceeeeiiiidnoooooouuuyybyRr                                 ';
+
+    $slug = str_replace(
+        ["-----", "----", "---", "--"],
+        "-",
+        str_replace(
+            " ",
+            "-",
+            trim(strtr(utf8_decode($string), utf8_decode($formats), $replace))
+        )
+    );
+    return $slug;
+}
+
+/**
+ * @param string $string
+ * @return string
+ */
+function str_studly_case(string $string): string
+{
+    $string = str_slug($string);
+    $studlyCase = str_replace(
+        " ",
+        "",
+        mb_convert_case(str_replace("-", " ", $string), MB_CASE_TITLE)
+    );
+
+    return $studlyCase;
+}
+
+/**
+ * @param string $string
+ * @return string
+ */
+function str_camel_case(string $string): string
+{
+    return lcfirst(str_studly_case($string));
+}
+
+/**
+ * @param string $string
+ * @return string
+ */
+function str_title(string $string): string
+{
+    return mb_convert_case(filter_var($string, FILTER_SANITIZE_SPECIAL_CHARS), MB_CASE_TITLE);
+}
+
+/**
+ * @param string $text
+ * @return string
+ */
+function str_textarea(string $text): string
+{
+    $text = filter_var($text, FILTER_SANITIZE_STRIPPED);
+    $arrayReplace = ["&#10;", "&#10;&#10;", "&#10;&#10;&#10;", "&#10;&#10;&#10;&#10;", "&#10;&#10;&#10;&#10;&#10;"];
+    return "<p>" . str_replace($arrayReplace, "</p><p>", $text) . "</p>";
+}
+
+/**
+ * @param string $string
+ * @param int $limit
+ * @param string $pointer
+ * @return string
+ */
+function str_limit_words(string $string, int $limit, string $pointer = "..."): string
+{
+    $string = trim(filter_var($string, FILTER_SANITIZE_SPECIAL_CHARS));
+    $arrWords = explode(" ", $string);
+    $numWords = count($arrWords);
+
+    if ($numWords < $limit) {
+        return $string;
+    }
+
+    $words = implode(" ", array_slice($arrWords, 0, $limit));
+    return "{$words}{$pointer}";
+}
+
+/**
+ * @param string $string
+ * @param int $limit
+ * @param string $pointer
+ * @return string
+ */
+function str_limit_chars(string $string, int $limit, string $pointer = "..."): string
+{
+    $string = trim(filter_var($string, FILTER_SANITIZE_SPECIAL_CHARS));
+    if (mb_strlen($string) <= $limit) {
+        return $string;
+    }
+
+    $chars = mb_substr($string, 0, mb_strrpos(mb_substr($string, 0, $limit), " "));
+    return "{$chars}{$pointer}";
+}
+
+/**
+ * @param string $price
+ * @return string
+ */
+function str_price(?string $price): string
+{
+    return number_format((!empty($price) ? $price : 0), 2, ",", ".");
+}
+
+/**
+ * @param string|null $search
+ * @return string
+ */
+function str_search(?string $search): string
+{
+    if (!$search) {
+        return "all";
+    }
+
+    $search = preg_replace("/[^a-z0-9A-Z\@\ ]/", "", $search);
+    return (!empty($search) ? $search : "all");
+}
+
+function str_message_info(string $message): string
+{
+    return
+        "<div class=\"text-center\">" .
+            "<h1><i class=\"fa fa-info-circle text-info\"></i></h1>" .
+            "<h3>Conteúdo Vazio</h3>" .
+            "<p class=\"h5\">{$message}</p>" .
+        "</div>";
+}
+
+/**
+ * ###############
+ * ###   URL   ###
+ * ###############
+ */
+
+/**
+ * @param string $path
+ * @return string
+ */
+function url(string $path = null): string
+{
+    if (strpos($_SERVER['HTTP_HOST'], "localhost")) {
+        if ($path) {
+            return CONF_URL_TEST . "/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
+        }
+        return CONF_URL_TEST;
+    }
+
+    if ($path) {
+        return CONF_URL_BASE . "/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
+    }
+
+    return CONF_URL_BASE;
+}
+
+/**
+ * @return string
+ */
+function url_back(): string
+{
+    return ($_SERVER['HTTP_REFERER'] ?? url());
+}
+
+/**
+ * @param string $url
+ */
+function redirect(string $url): void
+{
+    header("HTTP/1.1 302 Redirect");
+    if (filter_var($url, FILTER_VALIDATE_URL)) {
+        header("Location: {$url}");
+        exit;
+    }
+
+    if (filter_input(INPUT_GET, "route", FILTER_DEFAULT) != $url) {
+        $location = url($url);
+        header("Location: {$location}");
+        exit;
+    }
+}
+
+/**
+ * ##################
+ * ###   ASSETS   ###
+ * ##################
+ */
+
+/**
+ * @return \Source\Models\User|null
+ */
+function user(): ?\Source\Models\User
+{
+    return \Source\Models\Auth::user();
+}
+
+/**
+ * @return \Source\Core\Session
+ */
+function session(): \Source\Core\Session
+{
+    return new \Source\Core\Session();
+}
+
+/**
+ * @param string|null $path
+ * @param string $theme
+ * @return string
+ */
+function theme(string $path = null, string $theme = CONF_VIEW_THEME): string
+{
+    if (strpos($_SERVER['HTTP_HOST'], "localhost")) {
+        if ($path) {
+            return CONF_URL_TEST . "/themes/{$theme}/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
+        }
+
+        return CONF_URL_TEST . "/themes/{$theme}";
+    }
+
+    if ($path) {
+        return CONF_URL_BASE . "/themes/{$theme}/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
+    }
+
+    return CONF_URL_BASE . "/themes/{$theme}";
+}
+
+/**
+ * @param string $image
+ * @param int $width
+ * @param int|null $height
+ * @return string
+ */
+function image(?string $image, int $width, int $height = null): ?string
+{
+    if ($image) {
+        return url() . "/" . (new \Source\Support\Thumb())->make($image, $width, $height);
+    }
+
+    return null;
+}
+
+/**
+ * ################
+ * ###   DATE   ###
+ * ################
+ */
+
+/**
+ * @param string $date
+ * @param string $format
+ * @return string
+ * @throws Exception
+ */
+function date_fmt(?string $date, string $format = "d/m/Y à\s H:i"): string
+{
+    $date = (empty($date) ? "now" : $date);
+    return (new DateTime($date))->format($format);
+}
+
+/**
+ * @param string $date
+ * @return string
+ * @throws Exception
+ */
+function date_fmt_br(?string $date): string
+{
+    $date = (empty($date) ? "now" : $date);
+    return (new DateTime($date))->format(CONF_DATE_BR);
+}
+
+/**
+ * @param string $date
+ * @return string
+ * @throws Exception
+ */
+function date_fmt_app(?string $date): string
+{
+    $date = (empty($date) ? "now" : $date);
+    return (new DateTime($date))->format(CONF_DATE_APP);
+}
+
+/**
+ * @param string|null $date
+ * @return string|null
+ */
+function date_fmt_back(?string $date): ?string
+{
+    if (!$date) {
+        return null;
+    }
+
+    if (strpos($date, " ")) {
+        $date = explode(" ", $date);
+        return implode("-", array_reverse(explode("/", $date[0]))) . " " . $date[1];
+    }
+
+    return implode("-", array_reverse(explode("/", $date)));
+}
+
+/**
+ * ####################
+ * ###   PASSWORD   ###
+ * ####################
+ */
+
+/**
+ * @param string $password
+ * @return string
+ */
+function passwd(string $password): string
+{
+    if (!empty(password_get_info($password)['algo'])) {
+        return $password;
+    }
+
+    return password_hash($password, CONF_PASSWD_ALGO, CONF_PASSWD_OPTION);
+}
+
+/**
+ * @param string $password
+ * @param string $hash
+ * @return bool
+ */
+function passwd_verify(string $password, string $hash): bool
+{
+    return password_verify($password, $hash);
+}
+
+/**
+ * @param string $hash
+ * @return bool
+ */
+function passwd_rehash(string $hash): bool
+{
+    return password_needs_rehash($hash, CONF_PASSWD_ALGO, CONF_PASSWD_OPTION);
+}
+
+/**
+ * ###################
+ * ###   REQUEST   ###
+ * ###################
+ */
+
+/**
+ * @return string
+ */
+function csrf_input(): string
+{
+    $session = new \Source\Core\Session();
+    $session->csrf();
+    return "<input type='hidden' name='csrf' value='" . ($session->csrf_token ?? "") . "'/>";
+}
+
+/**
+ * @param $request
+ * @return bool
+ */
+function csrf_verify($request): bool
+{
+    $session = new \Source\Core\Session();
+    if (empty($session->csrf_token) || empty($request['csrf']) || $request['csrf'] != $session->csrf_token) {
+        return false;
+    }
+    return true;
+}
+
+/**
+ * @return null|string
+ */
+function flash(): ?string
+{
+    $session = new \Source\Core\Session();
+    if ($flash = $session->flash()) {
+        return $flash;
+    }
+    return null;
+}
+
+/**
+ * @param string $key
+ * @param int $limit
+ * @param int $seconds
+ * @return bool
+ */
+function request_limit(string $key, int $limit = 5, int $seconds = 60): bool
+{
+    $session = new \Source\Core\Session();
+    if ($session->has($key) && $session->$key->time >= time() && $session->$key->requests < $limit) {
+        $session->set($key, [
+            "time" => time() + $seconds,
+            "requests" => $session->$key->requests + 1
+        ]);
+        return false;
+    }
+
+    if ($session->has($key) && $session->$key->time >= time() && $session->$key->requests >= $limit) {
+        return true;
+    }
+
+    $session->set($key, [
+        "time" => time() + $seconds,
+        "requests" => 1
+    ]);
+
+    return false;
+}
+
+/**
+ * @param string $field
+ * @param string $value
+ * @return bool
+ */
+function request_repeat(string $field, string $value): bool
+{
+    $session = new \Source\Core\Session();
+    if ($session->has($field) && $session->$field == $value) {
+        return true;
+    }
+
+    $session->set($field, $value);
+    return false;
+}
